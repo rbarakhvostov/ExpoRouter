bc4eb3510cb8093b49d32cbc5e41f919460533f1
rbarakhvostov <rbarakhvostov@gmail.com>
2023-06-17 17:55:56 +0300

    add Modal

--- app/_layout.js
+++ app/_layout.js
@@ -1,22 +1,26 @@
-import { Stack } from "expo-router"
+import { Stack, useRouter } from "expo-router"
 import { AntDesign } from '@expo/vector-icons'
-import { Alert } from "react-native"
 
-export default () => (
-  <Stack
-    screenOptions={{
-      headerTintColor: '#ffc8dd',
-      headerStyle: { backgroundColor: '#000000' },
-      headerTitleStyle: { fontWeight: 'bold' },
-      headerRight: () => (
-        <AntDesign
-          name="infocirlceo"
-          size={24} color="#ffffff"
-          onPress={() => Alert.alert('info')}
-        />
-      )
-    }}
-  >
-    <Stack.Screen name="index" options={{ title: 'Home' }} />
-  </Stack>
-)
+export default () => {
+ const router = useRouter()
+
+  return (
+    <Stack
+      screenOptions={{
+        headerTintColor: '#ffc8dd',
+        headerStyle: { backgroundColor: '#000000' },
+        headerTitleStyle: { fontWeight: 'bold' },
+        headerRight: () => (
+          <AntDesign
+            name="infocirlceo"
+            size={24} color="#ffffff"
+            onPress={() => router.push('/modal')}
+          />
+        )
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+    </Stack>
+  )
+}
